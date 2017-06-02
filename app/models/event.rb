dc570c9ddd70e8789c9d6ab2286173d237cff842
robcelin <rob.celin@kellogg.northwestern.edu>
2017-06-02 12:18:34 -0500

    Before Date/Time Inputs

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,12 +2,10 @@ class Event < ApplicationRecord
   validates :ticket_face_value, :numericality => true
   validates :ticket_face_value, :presence => true
   validates :start_time, :presence => true
-  validates :start_date, :presence => true
   validates :name, :presence => true
   validates :name, :uniqueness => { :case_sensitive => true }
   validates :event_description, :presence => true
   validates :end_time, :presence => true
-  validates :end_date, :presence => true
   validates :campusgroups_url, :presence => true
   validates :campusgroups_url, :uniqueness => true
   validates :address, :presence => true
